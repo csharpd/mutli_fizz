ce70164d8220c684e9ba6d94709d8a4486659217
Chloe Donegan <chloedonegan@gmail.com>
2014-10-14 17:37:45 +0100

    counter tests done

--- java_fizz/javascript_fizz/spec/JavabuzzSpec.js
+++ java_fizz/javascript_fizz/spec/JavabuzzSpec.js
@@ -2,19 +2,33 @@ describe('Javabuzz', function() {
 
 	var javabuzz;
 // this declares a local variable. Prevents a global one from being created
+	beforeEach(function() {
+		javabuzz = new Javabuzz();
+	});
 	describe('knows when a number is divisible by ', function() {
 		it('3', function() {
-			javabuzz = new Javabuzz();
 			// we need an instance of javabuzz to test against
 			expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
 			// we need to tell javascript explicitly that the isDivisibleByThree method belongs to the javabuzz class
 		});
+		it('5',function() {
+			expect(javabuzz.isDivisibleByFive(5)).toEqual(true);
+		});
+		it('15',function() {
+			expect(javabuzz.isDivisibleByFifteen(15)).toEqual(true);
+		});
 	});
+
 	describe('knows when a number is NOT', function() {
 
 	  it('divisible by 3', function() {
-	    javabuzz = new Javabuzz();
 	    expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+	  });
+	  it('divisible by 5',function() {
+			expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+	  });
+	  it('divisible by 15',function(){
+	  	expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
 	  });
 	});
 });
