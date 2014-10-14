18f690ecc3d27d49ca583bba2129872018f75184
Chloe Donegan <chloedonegan@gmail.com>
2014-10-14 17:56:57 +0100

    Java BUzz complete

--- java_fizz/javascript_fizz/src/Javabuzz.js
+++ java_fizz/javascript_fizz/src/Javabuzz.js
@@ -1,14 +1,28 @@
+//this is a class
 function Javabuzz(){};
 
+//these are methods
+Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
+	return (number % divisor === 0 );
+};
+
 Javabuzz.prototype.isDivisibleByThree = function(number) {
-  return (number % 3 === 0);
+  return this._isDivisibleBy(number, 3);
 };
 
 Javabuzz.prototype.isDivisibleByFive = function(number) {
-	return (number % 5 === 0);
+	return this._isDivisibleBy(number, 5);
 };
 
 Javabuzz.prototype.isDivisibleByFifteen = function(number) {
-	return (number % 15 === 0);
+	return this._isDivisibleBy(number, 15);
+	// need to tell javascript that _isDivisibleBy belongs to the javascript class. As we are already inside the class we just need to add this. to the the begining of our method.
+};
+
+Javabuzz.prototype.says = function(number) {
+	if (this.isDivisibleByFifteen(number)) return "Javabuzz";
+	if (this.isDivisibleByThree(number)) return "Java";
+	if (this.isDivisibleByFive(number)) return "Buzz";
+	return number;
 };
 
